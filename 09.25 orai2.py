528a82eb075af0bb655754aea32ebb9e6dff66ba
speizerab <speizer.abel.tech2022b@bolyaimovar.com>
2023-09-25 14:56:01 +0200

    a

---
+++ 09.25 orai2.py
@@ -0,0 +1,7 @@
+a=input("írjon be egy szót:")
+b="alma"
+szo = input("írjon be egy szót:")
+if szo:
+    print("Ez egy szép szó!")
+if
+    print("A program futás véget ért, mert megadta a kulcsszót.")
---
+++ orai 09.25.py
@@ -0,0 +1,22 @@
+a=int(input(Kérem adja meg az 'a' oldalt:"))
+
+# a=int(input(Kérem adja meg az 'a' oldalt:"))
+# a=int(input(Kérem adja meg az 'a' oldalt:"))
+# a=int(input(Kérem adja meg az 'a' oldalt:"))
+#
+#
+
+# feladat haromszog oldalai es kerulet
+# k=0
+# for i in range(3):
+#     a=int(input(f"Kérem adja meg az {i+1}. oldalt:"))
+#     k=k+a
+#
+# print("A háromszög kerülete:",k)
+i=1
+a=0
+while a<1:
+    a=int(input(f"{i+1}Kérem adja meg egy pozitív számot:"))
+    i+=1
+
+print(a)
