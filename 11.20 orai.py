38c978d33d67c5f4b2774ec7d4d63fcdbb23e1db
speizerab <speizer.abel.tech2022b@bolyaimovar.com>
2023-11-20 14:51:15 +0100

    a

---
+++ 11.20 orai.py
@@ -0,0 +1,55 @@
+# t2=['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+# for i in range(len(t2)):
+#     print(t2[i])
+# for i in t2:
+#     print(i)
+# i=0
+#
+# while i < len(t2)+1:
+#     print(t2[i])
+#     i+=1
+# utolso=t2[-1]
+# for honap in t2:
+#     if honap!=utolso:
+#         print(honap+"*",end="")
+#     else:
+#         print(honap)
+#szamok=[32,  5, 12, 8, 35, 75, 2, 15]
+#rendez=[]
+
+#max=szamok[0]
+#while len(szamok)!=1:
+#    for szam in szamok:
+#        if szam>max:
+#            max=szam
+#    print(max)
+#    rendez.append(max)
+#    szamok.remove(max)
+#print(rendez)
+
+#t1= [32, 12, 8, 2, 5, 3, 75, 15]
+#paros=[]
+#paratlan=[]
+#for szam in t1:
+#    if szam%2==0:
+#        paros.append(szam)
+        #    else:
+#        paratlan.append(szam)
+#print("Páros:",paros)
+#print("Páratlan:",paratlan)
+
+
+#szamok=[]
+#szam=int(input("Kérem a számot!"))
+#while szam!=0:
+#   szamok.append(szam)
+#    szam=int(input("Kérek egy számot:"))
+
+#for szam in szamok:
+#    print(szam,"----->",szam**2,szam**3,)
+
+tabla=[]
+for i in range(1,16):
+
+
+
